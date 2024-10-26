64acb4abc92607a6bbe1ee2fa8ca588a9572ffa5
marotate <6422782092@g.siit.tu.ac.th>
2024-10-26 23:22:16 +0700

    edit config

--- Firebaseconfig.ts
+++ Firebaseconfig.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,9 +10,11 @@ const firebaseConfig = {
   projectId: "bookstore-2abd0",
   storageBucket: "bookstore-2abd0.appspot.com",
   messagingSenderId: "364913230065",
-  appId: "1:364913230065:web:21d2458954ad9a087ea950"
+  appId: "1:364913230065:web:21d2458954ad9a087ea950",
+  databaseURL: "https://bookstore-2abd0-default-rtdb.firebaseio.com/books",
 };
 
 // Initialize Firebase
 export const FIREBASE_app = initializeApp(firebaseConfig);
 export const FIREBASE_auth = getAuth(FIREBASE_app);
+export const FIREBASE_db = getDatabase(FIREBASE_app);
