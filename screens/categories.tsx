6af1419af5fd958e6a6265856bf61e041989a0ba
marotate <6422782092@g.siit.tu.ac.th>
2024-10-27 15:53:51 +0700

    create home page

---
+++ screens/categories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CategoriesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Categories Page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+export default CategoriesScreen;
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,23 +1,22 @@
-// App.tsx
+// screens/home.tsx
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import HomeScreen from "./screens/home";
 
-export default function App() {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <HomeScreen />
-      <StatusBar style="auto" />
+      <Text>Welcome to the Home Screen</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#fff",
   },
 });
+
+export default HomeScreen;
